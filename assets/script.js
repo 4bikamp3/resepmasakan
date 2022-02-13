c7cd3d79972ffba4eb1cdb92e65eb0f165d2770f
ilham <ilhamsun06@gmail.com>
2022-02-13 20:32:14 +0700

    home

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+fetch('https://masak-apa-tomorisakura.vercel.app/api/recipes-length/?limit=3')
+  .then(response => response.json())
+  .then(function (resep) {
+    let data = resep.results
+
+    let cards = ''
+    data.forEach(rsp => {
+      cards += showCard(rsp)
+    });
+
+    let list = document.querySelector('.recipe-wrap')
+    list.innerHTML = cards
+  })
+
+
+function showCard(r) {
+  let key = r.key
+  let name = key.replace(/-/g, " ")
+
+  return `<div class="col-lg-4">
+            <div class="recipe-item">
+              <div class="img">
+                <img class="img-fluid w-100" src="${r.thumb}" alt="">
+              </div>
+              <div class="text">
+                <p class="title">${name}</p>
+                <p class="description">${r.title}....</p>
+              </div>
+            </div>
+          </div>`
+}
